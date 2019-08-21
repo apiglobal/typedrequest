14bdba09263e196689fd8e1e71e330e99bea6085
Phil Kunz <gitlab@philkunz.com>
2019-08-21 14:36:16 +0200

    fix(core): update

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { expect, tap } from '@pushrocks/tapbundle';
+import * as typedrequest from '../ts/index'
+
+tap.test('first test', async () => {
+  console.log(typedrequest.standardExport)
+})
+
+tap.start()
