9d9f67c91a4f28a9bd9114ef6cdf6f5793286269
Phil Kunz <gitlab@philkunz.com>
2020-06-16 15:17:46 +0000

    fix(core): update

--- ts/typedrequest.classes.typedresponseerror.ts
+++ ts/typedrequest.classes.typedresponseerror.ts
@@ -3,7 +3,7 @@ import * as plugins from './typedrequest.plugins';
 export class TypedResponseError {
   public errorText: string;
   public errorData: any;
-  constructor(errorTextArg: string, errorDataArg: any) {
+  constructor(errorTextArg: string, errorDataArg?: any) {
     this.errorText = errorTextArg;
     this.errorData = errorDataArg;
   }
