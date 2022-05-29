fcc75af1ff002f6ae3c54d425d37b3e46224fe68
Philipp Kunz <gitlab@philkunz.com>
2022-05-29 20:38:46 +0200

    fix(core): update

---
+++ ts/00_commitinfo_data.ts
@@ -0,0 +1,8 @@
+/**
+ * autocreated commitinfo by @pushrocks/commitinfo
+ */
+export const commitinfo = {
+  name: '@apiglobal/typedrequest',
+  version: '2.0.4',
+  description: 'make typed requests towards apis'
+}
